d18a4d7ee14194aaeae5246b17e99146f8fa6444
Mateusz Turzyński <mateusz.turzynski@aspiresys.com>
2019-11-17 20:34:29 +0100

    playground project

---
+++ FrontNet.Playground/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontNet.Playground
+{
+    public class PersonModel
+    {
+        [Required]
+        [Range(18, 60, ErrorMessage = "Either too old or too young :(")]
+        public int Age { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string FullName { get; set; }
+
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string RepeatPassword { get; set; }
+    }
+}
