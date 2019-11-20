ce3ca04b504f762451fe79a5e6bc659d311221be
Michal Michalczuk <michalczukm@gmail.com>
2019-11-20 23:57:39 +0100

    added example of BlazorStyled

--- FrontNet.Playground/Startup.cs
+++ FrontNet.Playground/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorStyled;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,11 +8,13 @@ namespace FrontNet.Playground
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazorStyled();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
+            app.AddComponent<ClientSideStyled>("#styled");
         }
     }
 }
